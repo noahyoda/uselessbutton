51057a49791ebb13f77d7e59edeb2c658bdd4efd
Noah Jaussi <noahyoda1@gmail.com>
2021-05-28 17:31:28 -1000

    functional main button and started incrementor

---
+++ uselessbutton/src/Components/Button.js
@@ -0,0 +1,26 @@
+import './Button.css';
+
+const ButtonActual = () => {
+    const buttonCount = () => {}
+
+    const mainButtonHandler = () => {
+        console.log("bruh moment");
+    }
+
+    return(
+        <div>
+            <div className="redbutton" onClick={mainButtonHandler}/>
+            <div className="count">{buttonCount}</div>
+            <div className="increment-up">
+                <div className="triangle-up"></div>
+            </div>
+            <div className="increment-down">
+                <div className="triangle-down"></div>
+            </div>
+        </div>
+    );
+};
+
+
+
+export default ButtonActual;
---
+++ uselessbutton/src/UI/Card.js
@@ -0,0 +1,13 @@
+import './Card.css';
+import Button from '../Components/Button';
+
+const Card = () => {
+
+    return(
+        <div className="card">
+            <Button/>
+        </div>
+    );
+}
+
+export default Card;
